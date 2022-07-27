7c395aace68611fda509b0132d123066108363cd
Denis <den88000@gmail.com>
2022-07-27 16:51:37 +0300

    delete spaces

--- HomeWork-10/ViewController.swift
+++ HomeWork-10/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  HomeWork-10
-//
-//  Created by Денис Холодков on 27.07.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,9 +5,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemRed
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
